050cc257755dbf2d730695ae232a5cc860c8e674
maximemb <mmboudot@gmail.com>
2026-03-05 15:52:29 +0100

    fix(core): restrict /error/ debug endpoint to DEBUG mode only

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -14,7 +15,10 @@ def trigger_error(request):
 
 urlpatterns = [
     path('health/', health_check, name='health_check'),
-    path('error/', trigger_error, name='trigger_error'),
+    *(
+        [path('error/', trigger_error, name='trigger_error')]
+        if settings.DEBUG else []
+    ),
     path('admin/', admin.site.urls),
     path('api/', include('todo.categories.urls')),
     path('api/', include('todo.tasks.urls')),
