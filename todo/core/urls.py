20d57af08ab106e90c66f830b30b14dba081255c
maximemb <mmboudot@gmail.com>
2026-03-05 16:19:22 +0100

    remove(core): drop sentry debug endpoint /sentry-debug/

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -8,14 +7,8 @@ def health_check(request):
     return JsonResponse({"status": "ok", "message": "API is healthy"})
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-    return JsonResponse({"this": "will never be returned"})
-
-
 urlpatterns = [
     path('health/', health_check, name='health_check'),
-    path('sentry-debug/', trigger_error, name='trigger_error'),
     path('admin/', admin.site.urls),
     path('api/', include('todo.categories.urls')),
     path('api/', include('todo.tasks.urls')),
